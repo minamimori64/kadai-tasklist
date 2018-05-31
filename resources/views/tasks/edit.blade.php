0838608b97bf683d11b10d32bb817edabc899777
minami.mori <minamimori0604@gmail.com>
2018-05-31 04:27:47 +0000

    bootstrap design

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,18 +3,21 @@
 @section('content')
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
-
+<div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
         
-        
+        <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-        
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
         {!! Form::submit('更新') !!}
 
     {!! Form::close() !!}
-
+        </div>
+</div>
 @endsection
